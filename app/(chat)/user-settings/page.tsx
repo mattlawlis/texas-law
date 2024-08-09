68765a34d450d31a9d3ab74e0be392e22934e7ed
mattlawlis <89171568+mattlawlis@users.noreply.github.com>
2024-08-08 22:47:32 -0500

    Create page.tsx

---
+++ app/(chat)/user-settings/page.tsx
@@ -0,0 +1,12 @@
+import SubscriptionManager from '@/components/SubscriptionManager';
+
+const UserSettings = () => {
+  return (
+    <div>
+      <h1>User Settings</h1>
+      <SubscriptionManager />
+    </div>
+  );
+};
+
+export default UserSettings;
