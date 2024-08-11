50eb9b50f702dc5f901ded386288c1f27b23008e
Matthew Lawlis <matt@lawlis.me>
2024-08-11 03:23:38 -0500

    Add polyfills for Node.js core modules in Next.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,38 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '**'
-      }
-    ]
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      console.log('Applying client-side fallbacks');
-      config.resolve.fallback = {
-        crypto: require.resolve('crypto-browserify'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-      };
-    }
+   /** @type {import('next').NextConfig} */
+   module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+          port: '',
+          pathname: '**'
+        }
+      ]
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        console.log('Applying client-side fallbacks');
+        config.resolve.fallback = {
+          crypto: require.resolve('crypto-browserify'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+        };
 
-    return config;
-  },
-}
+        // Add polyfills to the entry point
+        config.entry = async () => {
+          const entries = await config.entry();
+          if (entries['main.js']) {
+            entries['main.js'].unshift(
+              require.resolve('crypto-browserify'),
+              require.resolve('stream-http'),
+              require.resolve('https-browserify')
+            );
+          }
+          return entries;
+        };
+      }
+
+      return config;
+    },
+  }
