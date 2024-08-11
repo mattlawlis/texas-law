ed31e28c501c7985b0bffbe1185f1db68f3e5cd1
Matthew Lawlis <matt@lawlis.me>
2024-08-11 03:29:39 -0500

    Add explicit polyfills for Node.js core modules

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+};
